6e6c624eff7dacf6e26eeebd745e07067f877098
TehcJS <tehcdev@gmail.com>
2022-05-19 16:32:17 -0400

    use tabs

--- src/main/kotlin/dev/cursedmc/ac/features/circuit/PowerManager.kt
+++ src/main/kotlin/dev/cursedmc/ac/features/circuit/PowerManager.kt
@@ -13,40 +13,40 @@ import net.minecraft.util.registry.RegistryKey
 import net.minecraft.world.World
 
 object PowerManager : Initializable {
-    private val graphsToUpdate = mutableMapOf<RegistryKey<World>, LongSet>()
-
-    override fun initialize() {
-        ServerTickEvents.END_WORLD_TICK.register(::doAllUpdates)
-    }
-
-    fun scheduleUpdate(world: ServerWorld, pos: BlockPos) {
-        val setForWorld = graphsToUpdate.computeIfAbsent(world.registryKey) { LongLinkedOpenHashSet() }
-        GraphLib.getController(world).getGraphsInPos(pos).forEach(setForWorld::add)
-    }
-    
-    private fun doAllUpdates(world: ServerWorld) {
-        graphsToUpdate[world.registryKey]?.let { setForWorld ->
-            for (id in setForWorld.iterator()) {
-                updateGraph(world, id)
-            }
-            setForWorld.clear()
-        }
-    }
-    
-    private fun updateGraph(world: ServerWorld, id: Long) {
-        val controller = GraphLib.getController(world)
-        
-        // Sometimes graphs that were registered for an update will have been removed by the time this has been called,
-        // like if the graph was merged with another graph, but that would mean the other graph would also be scheduled
-        // for an update as well, so it would be ok.
-        val network = controller.getGraph(id) ?: return
-
-        val power = network.nodes
-            .anyMatch { 
-                // loop through all power sources and return as soon as we find one that's powered
-                val node = it.node
-                node is PowerCarrierNode && node.getInput(world, it)
-            }
+	private val graphsToUpdate = mutableMapOf<RegistryKey<World>, LongSet>()
+	
+	override fun initialize() {
+		ServerTickEvents.END_WORLD_TICK.register(::doAllUpdates)
+	}
+	
+	fun scheduleUpdate(world: ServerWorld, pos: BlockPos) {
+		val setForWorld = graphsToUpdate.computeIfAbsent(world.registryKey) { LongLinkedOpenHashSet() }
+		GraphLib.getController(world).getGraphsInPos(pos).forEach(setForWorld::add)
+	}
+	
+	private fun doAllUpdates(world: ServerWorld) {
+		graphsToUpdate[world.registryKey]?.let { setForWorld ->
+			for (id in setForWorld.iterator()) {
+				updateGraph(world, id)
+			}
+			setForWorld.clear()
+		}
+	}
+	
+	private fun updateGraph(world: ServerWorld, id: Long) {
+		val controller = GraphLib.getController(world)
+		
+		// Sometimes graphs that were registered for an update will have been removed by the time this has been called,
+		// like if the graph was merged with another graph, but that would mean the other graph would also be scheduled
+		// for an update as well, so it would be ok.
+		val network = controller.getGraph(id) ?: return
+		
+		val power = network.nodes
+			.anyMatch {
+				// loop through all power sources and return as soon as we find one that's powered
+				val node = it.node
+				node is PowerCarrierNode && node.getInput(world, it)
+			}
 //            .limit(512)
 //            .reduce { node, node2 ->
 //                // get all power sources on the network and say we're powered if one of them is.
@@ -58,9 +58,9 @@ object PowerManager : Initializable {
 //            }.run {
 //                return@run (this.node as PowerCarrierNode).getInput(world, this)
 //            }
-        network.nodes
-            .forEach {
-                (it.node as? PowerCarrierNode)?.setPower(world, it, power)
-            }
-    }
+		network.nodes
+			.forEach {
+				(it.node as? PowerCarrierNode)?.setPower(world, it, power)
+			}
+	}
 }
--- src/main/kotlin/dev/cursedmc/ac/features/circuit/block/node/BlockNodes.kt
+++ src/main/kotlin/dev/cursedmc/ac/features/circuit/block/node/BlockNodes.kt
@@ -7,9 +7,9 @@ import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
 object BlockNodes : Initializable {
-    val WIRE_BLOCK_NODE: Identifier = Blocks.WIRE_BLOCK.first.lootTableId
-
-    override fun initialize() {
-        Registry.register(GraphLib.BLOCK_NODE_DECODER, WIRE_BLOCK_NODE, WireBlockNode.Decoder)
-    }
+	val WIRE_BLOCK_NODE: Identifier = Blocks.WIRE_BLOCK.first.lootTableId
+	
+	override fun initialize() {
+		Registry.register(GraphLib.BLOCK_NODE_DECODER, WIRE_BLOCK_NODE, WireBlockNode.Decoder)
+	}
 }
