daecb64bd763162b3ea7cbe28b2ff041e67c9b64
TehcJS <tehcdev@gmail.com>
2022-05-19 16:26:39 -0400

    add doc to Initializable.kt

--- src/main/kotlin/dev/cursedmc/ac/features/Initializable.kt
+++ src/main/kotlin/dev/cursedmc/ac/features/Initializable.kt
@@ -1,5 +1,8 @@
 package dev.cursedmc.ac.features
 
+/**
+ * Adds a no-op initialize function that automatically calls <code>&lt;clinit&gt;</code>.
+ */
 interface Initializable {
 	/* no-op */
 	fun initialize() = Unit
